821b95bffe681354b519502e01d96fcd2abac94b
Reval Fahmi <reval.fahmi42@gmail.com>
2022-05-12 16:32:19 +0700

    Splash Screen

---
+++ app/src/main/java/com/example/binar_chapter4/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.binar_chapter4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }, 5000)
+    }
+}
